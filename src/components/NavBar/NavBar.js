5b14b5893867774a736299b5bede6d7dcac21019
Yogesh4220 <atlayogeswararao@gmail.com>
2022-11-05 03:03:02 +0000

    :q

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+import {withRouter, Link} from 'react-router-dom'
+import MobileNavigation from './MobileNavigation'
+import Navigation from './Navigation'
+
+import classes from './NavBar.Module.css'
+
+const logoUrl =
+  'https://res.cloudinary.com/dppqkea7f/image/upload/v1625742512/Frame_274_zlrzwk.svg'
+
+const NavBar = () => (
+  <div className={classes.NavBar}>
+    <Link to="/" style={{textDecoration: 'none'}}>
+      <div className={classes.LogoContainer}>
+        <img src={logoUrl} alt="website logo" className={classes.LogoImage} />
+        <h1 className={classes.MainHeading}>Tasty Kitchens</h1>
+      </div>
+    </Link>
+    <Navigation />
+    <MobileNavigation />
+  </div>
+)
+
+export default withRouter(NavBar)
